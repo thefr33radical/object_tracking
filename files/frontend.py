5c0a0d9ff1e2c170476c103533327d409198b8f3
user <imperial.gauntlet@gmail.com>
2019-06-13 18:21:24 -0400

    frontend updated

---
+++ files/frontend.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import os
+
+
+root_path = "/home/user/workdir/thefr33radical/object_tracking/template"
+app = Flask(__name__,template_folder=root_path,root_path=root_path)
+
+
+@app.route("/")
+def home():
+    print(os.getcwd())
+    return render_template("index.html")
+
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
