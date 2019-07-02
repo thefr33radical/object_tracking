dbeb3264a7736df2a08b3289008da6d909502890
thefr33radical <imperial.gauntlet@gmail.com>
2019-07-02 19:01:05 -0400

    updated error lines

--- files/object_tracker1.py
+++ files/object_tracker1.py
@@ -17,9 +17,14 @@ import logging
 import math
 
 
+
 class ObjectTracker(object):
     def __init__(self):
         self.set_val=1
+        self.mouseX=0
+        self.mouseY=0
+        self.mouse2X=0
+        self.mouse2Y=0
 
     def exit_routine(self,frame_counter, start,name,age,exp,speed_data,time_data,speed_data2,time_data2):
         end = time.time()
@@ -41,6 +46,15 @@ class ObjectTracker(object):
         print("FPS:",fps)
         output.to_csv("output_"+str(exp)+".csv")
 
+    def draw_circle(self,event,x,y,flags,param):
+        
+        print("--------------------------------")
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            self.mouseX,self.mouseY = x,y            
+          
+        if event == cv2.EVENT_RBUTTONDBLCLK:
+            self.mouse2X,self.mouse2Y = x,y
+       
     def compute(self,name,age,exp):
             
         path = "c:/Users/ReGameVR/Envs/regamevr_virtualenv/object_tracking/"
@@ -54,7 +68,10 @@ class ObjectTracker(object):
                                                 PyKinectV2.FrameSourceTypes_Depth | PyKinectV2.FrameSourceTypes_Infrared)
         # Default: 1920, 1080 video height and width for kinect
         color_width, color_height = kinect.color_frame_desc.Width, kinect.color_frame_desc.Height
+        cv2.namedWindow('Frame')
+        
         print("Frame_width, Frame_height", color_width, color_height)
+        
         circle_color = (0, 255, 255)
         trace_color = (0, 0, 255)
         start = 0.0
@@ -133,25 +150,23 @@ class ObjectTracker(object):
 
                 center = None
                 center_bat = None
+                
+                # Capture Mouse Clicks to form Error Line
+                cv2.setMouseCallback("Frame",self.draw_circle)
+                
+                # CASE 1
 
-                # case 1
-                # Find the contours of the bat and ball
-                if len(cnts) > 0 and len(cnts_bat)>0:
+                # Find the contours of ball
+                if len(cnts) > 0 :
                         # find the largest contour in the mask, then use
                         # it to compute the minimum enclosing circle and
-
-                    c = max(cnts, key=cv2.contourArea)
-                    c2 = max(cnts_bat,key=cv2.contourArea)
-                    
-                    ((x, y), radius) = cv2.minEnclosingCircle(c)
-                    ((x2, y2), radius2) = cv2.minEnclosingCircle(c2)
-
+                    c = max(cnts, key=cv2.contourArea)                    
+                    ((x, y), radius) = cv2.minEnclosingCircle(c)                    
                     M = cv2.moments(c)
-                    M2 = cv2.moments(c2)
-
+                    
                     # only proceed if the radius meets a minimum size
-                    if radius > 10 and radius2 >10:
-                        print("got both radius")
+                    if radius > 10:
+                        print("got ball radius")
                         # draw the circle and centroid on the frame,
                         # then update the list of tracked points
 
@@ -160,33 +175,26 @@ class ObjectTracker(object):
                         try:
                             # centroid, centre of the circle for ball
                             center = (int(M["m10"] / M["m00"]),
-                                    int(M["m01"] / M["m00"]))
-                            # centroid, centre of the circle for bat
-                            center2 = (int(M2["m10"] / M2["m00"]),
-                                    int(M2["m01"] / M2["m00"]))
+                                    int(M["m01"] / M["m00"]))                            
 
                             # Draw the outer circle on the ball
                             cv2.circle(frame, (int(x), int(y)),
-                                    int(radius), circle_color, 1)
-                            # Draw the outer circle on the bat
-                            cv2.circle(frame, (int(x2), int(y2)),
-                                    int(radius2), circle_color, 1)
+                                    int(radius), circle_color, 1)                            
 
                             # Draw the point circle on centroid and fill it
                             cv2.circle(frame, center, 5, trace_color, -1)
-                            cv2.circle(frame, center2, 5, trace_color, -1)
+                            pts.appendleft(center)                            
 
-                            pts.appendleft(center)
-                            bat_pts.appendleft(center2)
-
-                            if center[1] >150 and center[1] < 400:
+                            # Compute Error of Ball across the line
+                            if center[1] >self.mouseY and center[1] < self.mouse2Y:
                                 error.appendleft(0)
-                            if center[1] < 150:
-                                error.appendleft(150-center[1])
+                            if center[1] < self.mouseY:
+                                error.appendleft(self.mouseY-center[1])
                             else:
-                                error.appendleft(400-center[1])
+                                error.appendleft(self.mouse2Y-center[1])
 
-                            if len(time_q)>1 and len(dist_q) >1 and len(time_q2)>1 and len(dist_q) >1:
+                            # Compute Speed, Distance if previous data exists in Queue else insert First data into Queue
+                            if len(time_q)>1 and len(dist_q) >1 :
                                 d = dist_q.pop()
                                 t = time_q.pop()
                                 pr = time.time()
@@ -194,33 +202,15 @@ class ObjectTracker(object):
                                 speed =  abs(center[1]-d[1]) / abs(t - pr)
                                 time_q.appendleft(pr)
                                 dist_q.appendleft(center)
-
                                 speed_data.appendleft(speed)
-                                time_data.appendleft(pr)
-
-                                # Bat speed
-                                d2 = dist_q2.pop()
-                                t2 = time_q2.pop()
-                                pr2 = time.time()
-                                #speed =  math.sqrt( (center[0]-d[0])**2 + (center[1]-d[1])**2 ) / abs(t - pr)
-                                speed2 =  abs(center2[1]-d2[1]) / abs(t2 - pr2)
-                                time_q2.appendleft(pr2)
-                                dist2_q.appendleft(center2)
-
-                                speed_data2.appendleft(speed2)
-                                time_data2.appendleft(pr2)
-
+                                time_data.appendleft(pr)                          
                             else:
                                 speed_data.appendleft(0)
                                 time_data.appendleft(0)
                                 time_q.appendleft(time.time())
                                 dist_q.appendleft(center)
 
-                                speed_data2.appendleft(0)
-                                time_data2.appendleft(0)
-                                time_q2.appendleft(time.time())
-                                dist_q2.appendleft(center2)
-
+                            # Compute Trail of the Ball
                             for i in range(1, len(pts)):
                                 if pts[i - 1] is None or pts[i] is None:
                                     continue
@@ -231,40 +221,90 @@ class ObjectTracker(object):
                                 except Exception as e:
                                     logging.exception(
                                         "Computing error in line thickness ")
-
                         except Exception as e:
-                            logging.exception("Moments values are 0 error ")
-                
-                    # Update Frame Number
-                    frame_counter += 1
-                
-                
+                            logging.exception("Moments values are 0 error ")       
                 else:
-                    logging.warning("No ball and bat detected") 
-                    print('ball, bat not detected')           
-                    speed=0
-                    speed2=0
+                    logging.warning("No ball detected") 
+                    print('ball not detected')           
+                    speed=0  
+           
+                # CASE 2
+
+                # Find the contours of bat
+                if len(cnts_bat)>0 :
+                        # find the largest contour in the mask, then use
+                        # it to compute the minimum enclosing circle and
+                    c2 = max(cnts_bat,key=cv2.contourArea)                    
+                    ((x2, y2), radius2) = cv2.minEnclosingCircle(c2)                  
+                    M2 = cv2.moments(c2)
                     
+                    # only proceed if the radius meets a minimum size
+                    if radius2 >10:
+                        print("got bat radius")
+                        # draw the circle and centroid on the frame, for bat detected
+                        #                      
+                        try:
+                            # centroid, centre of the circle for bat
+                            center2 = (int(M2["m10"] / M2["m00"]),
+                                    int(M2["m01"] / M2["m00"]))                           
+
+                            # Draw the outer circle on the bat
+                            cv2.circle(frame, (int(x2), int(y2)),
+                                    int(radius2), circle_color, 1)                       
+
+                            # Draw the point circle on centroid and fill it
+                            cv2.circle(frame, center2, 5, trace_color, -1)
+                            bat_pts.appendleft(center2)                  
+
+                            # Compute Speed, Distance if previous data exists in Queue else insert First data into Queue
+                            if  len(time_q2)>1 and len(dist_q2) >1 :
+                                d2 = dist_q2.pop()
+                                t2 = time_q2.pop()
+                                pr2 = time.time()
+                                #speed =  math.sqrt( (center[0]-d[0])**2 + (center[1]-d[1])**2 ) / abs(t - pr)
+                                speed2 =  abs(center2[1]-d2[1]) / abs(t2 - pr2)
+                                time_q2.appendleft(pr2)
+                                dist2_q.appendleft(center2)
+                                speed_data2.appendleft(speed2)
+                                time_data2.appendleft(pr2)
+                            else:
+                                speed_data2.appendleft(0)
+                                time_data2.appendleft(0)
+                                time_q2.appendleft(time.time())
+                                dist_q2.appendleft(center2)                            
+                        except Exception as e:
+                            logging.exception("Moments values are 0 error ")       
+                else:
+                    logging.warning("No bat detected") 
+                    print('bat not detected')           
+                    speed2=0
+
+                # Update Frame Number    
+                frame_counter += 1    
                 current_time = time.time()
                 fps_present = float(frame_counter/(current_time-start))
-                cv2.putText(frame, 'FPS:'+str(fps_present),     position_text,
+
+                # Parameters to Display on Screen
+                cv2.putText(frame, 'FPS :'+str(fps_present),     position_text,
                                 font,     fontScale,    fontColor,    lineType)
                 
-                cv2.putText(frame, 'Ball Speed:'+str(speed),     position_text2,
+                cv2.putText(frame, 'Ball Speed :'+str(speed),     position_text2,
                                 font,     fontScale,    fontColor,    lineType)
               
-                cv2.putText(frame, 'Bat Speed:'+str(speed2),     bat_text2,
-                                font,     fontScale,    fontColor,    lineType)
-                cv2.putText(frame, 'Error:'+str(error),     error_text,
+                cv2.putText(frame, 'Bat Speed :'+str(speed2),     bat_text2,
                                 font,     fontScale,    fontColor,    lineType)
 
+                cv2.putText(frame, 'Error : '+str(error[0]),     error_text,
+                                font,     fontScale,    fontColor,    lineType)
+
+                # Draw Error Line
                 lineThickness = 2
-                cv2.line(frame, (0, 150), (960, 150), (0,255,0), lineThickness)
-                cv2.line(frame, (0, 400), (960, 400), (0,255,0), lineThickness)
-                # show the frame to our screen
+                cv2.line(frame, (0, self.mouseY), (960, self.mouseY), (0,255,0), lineThickness)
+                cv2.line(frame, (0, self.mouse2Y), (960, self.mouse2Y), (0,255,0), lineThickness)
                 
-                #frame = imutils.resize(frame, width=320,height =240)
+                # show the frame to screen
                 cv2.imshow("Frame", frame)
+                # Save Frame, img size should be equal to inp size
                 out.write(frame)
 
                 # Press Escape key to Quit
