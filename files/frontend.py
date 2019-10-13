e3dab40e3a6de37986e02208376de9501d3ec908
Gowtham Nayak <imperial.gauntlet@gmail.com>
2019-10-13 15:11:45 -0400

    Frontend Complete

--- files/frontend.py
+++ files/frontend.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template,request
 import os
 import pandas as pd
 os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from object_tracker1 import ObjectTracker
-
+from trail import Record
+import signal 
+import multiprocessing
+path = os.path.dirname(os.path.abspath(__file__))
+#exp cond : control, restricted, enhanced
+# seesion baseline, post aquistion
 # Set root path
 # set static path
 template_path =os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\template"
@@ -12,7 +16,7 @@ print("ROOT PATH",template_path)
 
 app = Flask(__name__,template_folder=template_path)
 app.static_folder=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\template\\static"
-obj=ObjectTracker()
+obj=Record()
 
 @app.route("/")
 def home():
@@ -22,23 +26,35 @@ def home():
 @app.route("/",methods=["POST"])
 def home_return():
     print("Site initialized")
-    
+    child_pid=None
+    p1=None
     try:
         if  request.form['action'] == 'Start':
-            name=request.form. get("name")
-            age=request.form. get("age")
-            exp=request.form. get("exp")
-            print(name,age,exp)
-            obj.compute(name,age,exp)
+            name=request.form.get("name")
+            exp=request.form.get("exp")
+            session=request.form.get("session")
+            print(name,exp,session) 
+            p1 = multiprocessing.Process(target=obj.compute(name,exp,session)) 
+            p1.start()
+            child_pid=p1.pid
+            with open (path+"\\data.txt","w+") as f:
+                f.write(str(child_pid))
+            print(child_pid)
+            p1.join()
+            #p1.close()
+            print(p1)
             return render_template("stop.html")
         elif  request.form['action'] == 'Stop':
-            obj.set_val=0
-            return render_template("index.html")
+            file=None
+            try:                
+                return render_template("index.html")
+            except:                        
+                return render_template("index.html")
+
         #text = request.form['Experiment Number']
     except Exception as e:
         print(e)
     return render_template("stop.html")
-
 
 @app.route("/test")
 def test():
