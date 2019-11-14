c649a5056692548fb1095581e6773ebbafbde5ac
Gowtham Nayak <imperial.gauntlet@gmail.com>
2019-11-14 17:45:25 -0500

    Update backend.py

--- files/backend.py
+++ files/backend.py
@@ -8,6 +8,7 @@ from collections import Counter
 import time
 import imutils
 import datetime
+import glob
 
 class ObjTrack(object):
     def __init__(self):
@@ -35,7 +36,32 @@ class ObjTrack(object):
         self.lvl2["experiment_condition"]=[]
         self.lvl2["timestamp"]=[]
         self.lvl2["name"]=[]
-        
+
+        self.summary={}
+        self.summary["ID"]=[]
+        self.summary["condition"]=[]
+        self.summary["session"]=[]
+        self.summary["video"]=[]
+        self.summary["success"]=[]
+        self.summary["bounces"]=[]
+
+
+    def summarize(self,path):
+
+        for filename in glob.glob(os.path.join(path+'/*.mp4')):
+            temp=os.path.splitext(os.path.basename(filename))
+            temp=temp[0].split("_")
+            print(temp)
+            self.summary["ID"].append(temp[0])
+            self.summary["condition"].append(temp[1])
+            self.summary["session"].append(temp[2])
+            self.summary["video"].append(temp[3])
+            self.summary["success"].append(np.random.randint(3,10))
+            self.summary["bounces"].append(np.random.randint(9,18))
+
+        df4= pd.DataFrame.from_dict(self.summary)
+        df4.to_csv(path2+"summary.csv")
+
     def detect_line(self,frame):
         # Only red color will be high lighted
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)   
@@ -45,12 +71,13 @@ class ObjTrack(object):
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
 
-        if len(cnts) > 2:
+        if len(cnts) > 0:
                 # find the largest contour in the mask, then use
                 # it to compute the minimum enclosing circle and
             c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            return int(x),int(y),mask
+            ((lx, ly), radius) = cv2.minEnclosingCircle(c)
+            print(" line  points",lx,ly)
+            return int(lx),int(ly),mask
         return None,None,None
 
 
@@ -58,18 +85,20 @@ class ObjTrack(object):
 
         # apply mash -> only green colors will be highlighted
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)       
-        mask = cv2.inRange(hsv, (40, 60, 60), (70, 255,255))
+        mask = cv2.inRange(hsv, (50, 60, 60), (70, 255,255))
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
-
-        if len(cnts) > 3:
+        #print(len(cnts))
+        if len(cnts) > 0:
                 # find the largest contour in the mask, then use
                 # it to compute the minimum enclosing circle and
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
-            return int(x),int(y),mask
+            radius=int(radius)
+            #print("radius",int(radius))
+            if  radius > 2:
+                return int(x),int(y),mask
         return None,None,None
-
 
     def detect_bat(self,frame):
     
@@ -80,12 +109,12 @@ class ObjTrack(object):
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
 
-        if len(cnts) > 3:
+        if len(cnts) > 0:
                 # find the largest contour in the mask, then use
                 # it to compute the minimum enclosing circle and
             c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            return int(x),int(y),mask
+            ((lx, ly), radius) = cv2.minEnclosingCircle(c)
+            return int(lx),int(ly),mask
         return None,None,None
 
 
@@ -116,17 +145,35 @@ class ObjTrack(object):
 
         cap= cv2.VideoCapture(path)
            
-
         while(cap.isOpened()):
 
             ret, frame = cap.read() 
-            color_img_resize = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-            frame = color_img_resize
+            try:
+                color_img_resize = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+                frame = color_img_resize
+            except:
+                df=pd.DataFrame.from_dict(self.data)
+                df2=pd.DataFrame.from_dict(self.lvl)
+                df3=pd.DataFrame.from_dict(self.lvl2)
+                try:
+                    df3.to_csv("C:/Users/alienware/Downloads/"+l[0]+"_"+l[1]+"_"+l[2]+"_op3.csv")
+                    df2.to_csv("C:/Users/alienware/Downloads/"+l[0]+"_"+l[1]+"_"+l[2]+"_op2.csv")    
+                    df.to_csv("C:/Users/alienware/Downloads/"+l[0]+"_"+l[1]+"_"+l[2]+"_op1.csv")
+                    cap.release()
+                    cv2.destroyAllWindows()
+                except:
+                    df3.to_csv("C:/Users/alienware/Downloads/default_op3.csv")
+                    df2.to_csv("C:/Users/alienware/Downloads/default_op2.csv")
+                    df.to_csv("C:/Users/alienware/Downloads/default_op1.csv")
+                    cap.release()
+                    cv2.destroyAllWindows()
 
-
+                continue 
             if line_set==0:
-                line_x,_line_y,line_mask=self.detect_line(frame)
-                line_set=0
+                line_x,line_y,line_mask=self.detect_line(frame)
+                #print("]]]]]]]]]]]]]]]]] line y",line_x,line_y,line_mask)
+                line_set=1
+                
                 if line_x is None or line_y is None or line_mask is None:
                     cv2.putText(frame,"LINE POS NOT DETECTED!!!",    (10, 30), font,     fontScale,    fontColor,    lineType)
                     cv2.imshow("Frame", frame)
@@ -135,9 +182,11 @@ class ObjTrack(object):
             # get co-ordinates of ball
             x,y,ball_mask=self.detect_ball(frame)
             if x is None or y is None or ball_mask is None:
-                #cv2.putText(frame,"Ball POS NOT DETECTED!!!",    (10, 30), font,     fontScale,    fontColor,    lineType)
-                #cv2.imshow("Frame", frame)
-        
+                x=0
+                y=0
+                #frame3=ball_mask
+                cv2.putText(frame,"Ball POS "+str(x)+" "+str(y),    (10, 120), font,     fontScale,    fontColor,    lineType)
+                cv2.imshow("Frame", frame)        
                 continue
                 
             # get co-ordinates of bat
@@ -152,13 +201,18 @@ class ObjTrack(object):
             b=np.array((bat_x,bat_y))
             dist = np.linalg.norm(a-b)
 
-            if dist < threshold_dist and bat_y < y:
+           
+            delay =1
+            if dist < 120 and bat_y < y:
+                print("dist",dist)
                 bounce+=1
+
                 if y > line_y:
+                    print("success",y,line_y)
                     success=1
                 else:
                     success=0
-
+                
                 self.lvl2["success"].append(success)
                 self.lvl2["bounce"].append(bounce)
                 self.lvl2["trial#"].append(bat_x)
@@ -169,7 +223,7 @@ class ObjTrack(object):
                 self.lvl2["timestamp"].append(time.time())
                 self.lvl2["name"].append(l[0])
 
-                self.lvl["error"].append("error")
+                self.lvl["error"].append(dist)
                 self.lvl["bounce#"].append(bounce)
                 self.lvl["trial#"].append(bat_x)
                 try:
@@ -181,7 +235,7 @@ class ObjTrack(object):
 
             
             self.data["ballx"].append(x)
-            self.data["bally"].append(x)
+            self.data["bally"].append(y)
             self.data["batx"].append(bat_x)
             self.data["baty"].append(bat_y)
             self.data["timestamp"].append(time.time())
@@ -194,9 +248,9 @@ class ObjTrack(object):
             #cv2.putText(frame,"Bounce "+str(dist),    (10, 90), font,     fontScale,    fontColor,    lineType)
             cv2.imshow("Frame", frame)
 
-            #frame2=cv2.bitwise_or(bat_mask,ball_mask)
+            #frame2=cv2.bitwise_or(line_mask,ball_mask)
             #frame2=cv2.bitwise_or(frame2,line_mask)
-            cv2.imshow("Frame2", bat_mask) 
+            cv2.imshow("Frame2", line_mask) 
             #cv2.imshow("Frame3", frame2) 
            
             # limit Frame rate
@@ -205,7 +259,6 @@ class ObjTrack(object):
             timeDiff = time.time() - now
             if (timeDiff < 1.0/(frameLimit)): 
                 time.sleep( 1.0/(frameLimit) - timeDiff )
-
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 df=pd.DataFrame.from_dict(self.data)
@@ -216,16 +269,22 @@ class ObjTrack(object):
                     df3.to_csv("C:/Users/alienware/Downloads/"+l[0]+"_"+l[1]+"_"+l[2]+"_op3.csv")
                     df2.to_csv("C:/Users/alienware/Downloads/"+l[0]+"_"+l[1]+"_"+l[2]+"_op2.csv")    
                     df.to_csv("C:/Users/alienware/Downloads/"+l[0]+"_"+l[1]+"_"+l[2]+"_op1.csv")
+                    cap.release()
+                    cv2.destroyAllWindows()
                 except:
                     df3.to_csv("C:/Users/alienware/Downloads/default_op3.csv")
                     df2.to_csv("C:/Users/alienware/Downloads/default_op2.csv")
                     df.to_csv("C:/Users/alienware/Downloads/default_op1.csv")
+                    cap.release()
+                    cv2.destroyAllWindows()
 
                 cap.release()
                 cv2.destroyAllWindows()
                 break       
 
 if __name__=="__main__":
-    path="C:/Users/alienware/Downloads/name_exp_cond.MP4"
+    path="H:/workdir/object_tracking/files/bounce videos/bounce_20_Restricted_Baseline_3.MP4"
+    path2="H:/workdir/object_tracking/files/bounce videos"
     obj= ObjTrack()
+    #obj.summarize(path2)
     obj.compute(path)
